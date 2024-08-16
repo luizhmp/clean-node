ec80ed7773deef6f3e9f5d83d7bcaf528d512099
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-08-16 10:00:08 -0300

    refactor: move success http response to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest, serverError } from "../../helpers/http-helper";
+import { badRequest, serverError, ok } from "../../helpers/http-helper";
 import {
   AddAccount,
   HttpResponse,
@@ -48,10 +48,7 @@ export class SignupController implements Controller {
         password,
       });
 
-      return {
-        statusCode: 200,
-        body: account,
-      };
+      return ok(account);
     } catch (error) {
       return serverError();
     }
