f188bd8bb65c17145b8269e7cdc9a7b129462ebe
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-08-19 10:36:18 -0300

    test: ensure DbAddAccount throws if Encrypter throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,3 +1,4 @@
+import { rejects } from "assert";
 import { Encrypter } from "../../protocols/encrypter";
 import { DbAddAccount } from "./db-add-account";
 
@@ -36,5 +37,22 @@ describe("DbAddAccount Usecase", () => {
     await sut.add(accountData);
 
     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
+  });
+
+  test("Should throw if Encrypter throws", async () => {
+    const { sut, encrypterStub } = makeSut();
+    jest
+      .spyOn(encrypterStub, "encrypt")
+      .mockReturnValueOnce(
+        new Promise((resolve, rejects) => rejects(new Error()))
+      );
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
   });
 });
