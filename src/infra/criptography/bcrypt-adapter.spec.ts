797cc1dde4d19102a7d35c76ff3ca900da998e93
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-08-19 11:46:38 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from "bcrypt";
 import { BcryptAdapter } from "./bcrypt-adapter";
 
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
 describe("BcryptAdapter", () => {
   test("Should call bcrypt with correct values", async () => {
     const SALT = 12;
@@ -9,5 +15,13 @@ describe("BcryptAdapter", () => {
     await sut.encrypt("any_value");
 
     expect(hashSpy).toHaveBeenCalledWith("any_value", SALT);
+  });
+
+  test("Should return a hash on success", async () => {
+    const SALT = 12;
+    const sut = new BcryptAdapter(SALT);
+    const hash = await sut.encrypt("any_value");
+
+    expect(hash).toBe("hash");
   });
 });
