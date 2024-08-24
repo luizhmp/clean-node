780b7a514ab1b120c8258d463d9217ec5c3d60dc
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-08-24 09:23:42 -0300

    refactor: add db to mongo helper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,9 +1,11 @@
-import { Collection, MongoClient } from "mongodb";
+import { Collection, MongoClient, MongoDBNamespace } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
+  db: null as MongoDBNamespace,
   async connect(uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL);
+    this.client = await MongoClient.connect(uri);
+    this.db = await this.client.db(globalThis.__MONGO_DB_NAME__);
   },
 
   async disconnect(): Promise<void> {
