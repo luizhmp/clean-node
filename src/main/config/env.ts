c6f54076e10d27e96265405226d81b53a471d597
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-09-03 17:24:57 -0300

    feat: connect to mongodb before app listen

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export default {
+  mongoUrl:
+    process.env.MONGO_URL ||
+    "mongodb+srv://luizhmp:Mjolnir%233@clean-node-api.bntpx.mongodb.net/?retryWrites=true&w=majority&appName=clean-node-api",
+  port: process.env.PORT || 5050,
+};
