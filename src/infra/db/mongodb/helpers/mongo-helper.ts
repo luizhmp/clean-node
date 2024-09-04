0b81bb55d8bcbcd415825994cae9dddc78295112
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-09-04 08:25:10 -0300

    feat: ensure mongodb reconnect if client is down

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -2,17 +2,24 @@ import { Collection, MongoClient, MongoDBNamespace } from "mongodb";
 
 export const MongoHelper = {
   client: null as MongoClient,
+  uri: null as string,
   db: null as MongoDBNamespace,
   async connect(uri: string): Promise<void> {
+    this.uri = uri;
     this.client = await MongoClient.connect(uri);
     this.db = await this.client.db(globalThis.__MONGO_DB_NAME__);
   },
 
   async disconnect(): Promise<void> {
     await this.client.close();
+    this.client = null;
   },
 
-  getCollection(name: string): Collection {
+  async getCollection(name: string): Promise<Collection> {
+    if (!this.client) {
+      await this.connect(this.uri);
+    }
+
     return this.client.db().collection(name);
   },
 
