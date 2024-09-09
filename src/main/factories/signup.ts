f45dae62257c07d62dea3e3a24c4ce86c376a71a
LUIZ.PINHEIRO <luiz.pinheiro@CNX-LUIZPINHEIRO.local>
2024-09-09 09:04:37 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from "../../data/usecases/add-account/db-add-account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
 import { AccountMongoRepository } from "../../infra/db/mongodb/account-repository/account-repository";
+import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
 import { SignupController } from "../../presentation/controllers/signup/signup";
 import { Controller } from "../../presentation/protocols";
 import { EmailValidatorAdapter } from "../../utils/email-validator-adapter";
@@ -16,6 +17,7 @@ export const makeSignupController = (): Controller => {
     emailValidatorAdapter,
     dbAddAccount
   );
+  const logMongoRepository = new LogMongoRepository();
 
-  return new LogControllerDecorator(signUpController);
+  return new LogControllerDecorator(signUpController, logMongoRepository);
 };
