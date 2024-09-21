7cf306104e4ad66dc5b570cccd1483888bfdf1ec
Luiz Henrique Pinheiro <luiz.hmp@gmail.com>
2024-09-21 12:31:17 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,4 +3,5 @@ export default {
     process.env.MONGO_URL ||
     "mongodb+srv://luizhmp:Mjolnir%233@clean-node-api.bntpx.mongodb.net/?retryWrites=true&w=majority&appName=clean-node-api",
   port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || "tj670==5H",
 };
